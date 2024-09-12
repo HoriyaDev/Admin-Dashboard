86160440a0243ae43e6a2f788703582146abce3c
HoriyaDev <you@example.com>
2024-09-12 16:16:29 +0500

    addded name of enetered person in header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState  , useEffect} from 'react'
 import { IoMdArrowDropdown } from "react-icons/io";
 import LogOut from '../logout/LogOut';
 
 const Header = () => {
   const [open , setOpen] = useState(false)
+  const [name , setName] = useState("")
+
+ 
+    //  const getName = JSON.parse(localStorage.getItem('user'))
+    //  console.log("🚀 ~ Header ~ getName:", getName)
+    //  setName(getName.fname)
+   useEffect(()=>{
+    const getName = JSON.parse(localStorage.getItem('user'))
+    console.log("🚀 ~ Header ~ getName:", getName)
+    setName(getName.fname)
+
+    console.log("🚀 ~ Header ~ name:", getName)
+   }, [])
+    
+
   const handleOpen = () =>{
+    
+
     setOpen(!open)
   }
   
@@ -17,7 +34,7 @@ const Header = () => {
         <div className='flex justify-evenly'>
           <img src='Profile.jpg' className='h-12 w-12 rounded-full object-cover m-2' alt="Profile"/>
           <div className='flex-col'>
-            <h1 className='text-2xl font-bold'>Taimoor Aslam</h1>
+            <h1 className='text-2xl font-bold'>{name}</h1>
             <p className='bg-blue-100 rounded-full w-20 text-center text-[12px]'>SuperAdmin</p>
           </div>
           <button type='button' onClick={handleOpen}>
