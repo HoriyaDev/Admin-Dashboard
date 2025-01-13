cdb1a719bebd36d7f716e0c4825860bc970aaa99
HoriyaDev <you@example.com>
2025-01-14 00:44:13 +0500

    hashrouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LoginPage from './Pages/login/LoginPage';
 import DashBoard from './Pages/dashboard/DashBoard';
 import Profile from './Pages/profile/Profile';  
 import {  Routes, Route } from "react-router-dom";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 
 import Register from './Pages/login/Register';
 import ProtectedRoute from './Services/ProtectedRoute';
